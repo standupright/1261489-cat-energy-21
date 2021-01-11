a9e0ee33580a9eb0b7c3ae542364cbd998bc8dee
standupright <standupright@mail.ru>
2021-01-11 16:41:58 +0500

    Добавил графику

---
+++ source/js/scripts.js
@@ -0,0 +1,15 @@
+var nav = document.querySelector('.nav');
+var navToggle = document.querySelector('.nav__toggle');
+
+// nav open and closed
+nav.classList.remove('nav--nojs');
+
+navToggle.addEventListener('click', function() {
+  if (nav.classList.contains('nav--closed')) {
+    nav.classList.remove('nav--closed');
+    nav.classList.add('nav--opened');
+  } else {
+    nav.classList.add('nav--closed');
+    nav.classList.remove('nav--opened');
+  }
+});
